826912b65ecce8fa5d11369fec3f2c4b04a0f536
Kyle Coots <snowballrandom@users.noreply.github.com>
2018-06-26 22:49:22 -0500

    Added reverse multiplcation function.

--- factorial.js
+++ factorial.js
@@ -1,11 +1,25 @@
 //Javascript
 
+//Forward Multiplcation
+// num = 4 
+// 1 x 2 x 3 x 4 = 24
 function factorial(num){
 	var result = 1;
-	for (var i = 1; i <= num; i++) {
+	for (var i = 2; i <= num; i++) {
 		result *= i;
 	}	
 	return result;
+}
+
+//Reverse Multiplcation
+// num = 4 
+// 4 x 3 x 2 x 1 = 24
+function factorialReverse(num){
+	var result = num;
+	for (var i = num-1; i >= 1; i--) {
+		result *= i;
+	}	
+	return result;	
 }
 
 document.getElementById("submit").onclick = function(){
